01ad9b54623c4752a8f9ed1c7442593ea234761e
moniljoshi <monil.joshi@qualitia.com>
2021-08-04 07:50:00 +0530

    Install Cypress and Created basic Test strucutre to write test

---
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -0,0 +1,9 @@
+describe('My First Test Suite',function(){
+    it('My First Test case',function(){
+        //test code
+    })
+    it('My second test',function(){
+        //test code
+    })
+
+})
