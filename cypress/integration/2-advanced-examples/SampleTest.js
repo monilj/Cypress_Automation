7460bcfb4f10c37957795ef4030ab36f93ebc22a
moniljoshi <monil.joshi@qualitia.com>
2021-08-06 17:20:57 +0530

    Added basic code to visit a website and execute it

--- cypress/integration/2-advanced-examples/SampleTest.js
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -1,9 +1,8 @@
-describe('My First Test Suite',function(){
-    it('My First Test case',function(){
-        //test code
-    })
-    it('My second test',function(){
-        //test code
-    })
-
-})
+describe("My First Test Suite", function () {
+  it("My First Test case", function () {
+    cy.visit("https://www.perfecto.io/");
+  });
+  it("My second test", function () {
+    //test code
+  });
+});
