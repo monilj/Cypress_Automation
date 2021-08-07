5382514e8dcfac7bd0f7a2bb33dd14ce27ab95e2
moniljoshi <monil.joshi@qualitia.com>
2021-08-07 23:46:48 +0530

    Added Basic test with Assertion, hadnling invisible element and Wait

--- cypress/integration/2-advanced-examples/SampleTest.js
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -1,6 +1,10 @@
+/// <reference types="cypress" />
 describe("My First Test Suite", function () {
   it("My First Test case", function () {
-    cy.visit("https://www.perfecto.io/");
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product:visible").should("have.length", 4);
   });
   it("My second test", function () {
     //test code
