5dcc0fd8f8b9c47b777b3ca7e284c411830d11b0
moniljoshi <monil.joshi@qualitia.com>
2021-08-08 00:23:47 +0530

    Use eq method to go for specifc element and contains method to find element by text

--- cypress/integration/2-advanced-examples/SampleTest.js
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -7,6 +7,7 @@ describe("My First Test Suite", function () {
     cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
   });
   it("My second test", function () {
     //test code
