b80b807f1ba3f1bdc3ee983cae7dc043c9e4b7da
moniljoshi <monil.joshi@qualitia.com>
2021-08-08 00:47:38 +0530

    Iterating over array elements using Each

--- cypress/integration/2-advanced-examples/SampleTest.js
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -7,7 +7,15 @@ describe("My First Test Suite", function () {
     cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
-    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+    // cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          $el.find("button").click();
+        }
+      });
   });
   it("My second test", function () {
     //test code
