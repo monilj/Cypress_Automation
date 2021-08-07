8df315f19d88f9b8631e21f2ee9461e37ebf9a27
moniljoshi <monil.joshi@qualitia.com>
2021-08-08 00:16:44 +0530

    Added method find which creates parent to child chaining

--- cypress/integration/2-advanced-examples/SampleTest.js
+++ cypress/integration/2-advanced-examples/SampleTest.js
@@ -5,6 +5,8 @@ describe("My First Test Suite", function () {
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
     cy.get(".product:visible").should("have.length", 4);
+    // Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
   });
   it("My second test", function () {
     //test code
