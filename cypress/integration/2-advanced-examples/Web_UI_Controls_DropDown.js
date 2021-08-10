664ef55059a67ed08f00ac525ffb4764a0d701fd
moniljoshi <monil.joshi@qualitia.com>
2021-08-10 23:42:54 +0530

    Refactored and restructured code for web UI controls

--- cypress/integration/2-advanced-examples/Web_UI_Controls_DropDown.js
+++ cypress/integration/2-advanced-examples/Web_UI_Controls_DropDown.js
@@ -1,15 +1,7 @@
 /// <reference types="cypress" />
 describe("Execution on Web Control", function () {
-  it("Web Control Checkbox", function () {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("#checkBoxOption1")
-      .check()
-      .should("be.checked")
-      .and("have.value", "option1");
-    cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
-    cy.get("input[type='checkbox']").check(["option2", "option3"]);
-  });
   it("Handling Static Drop downs", function () {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("select").select("option2").should("have.value", "option2");
   });
   it("Handling Dynamic Drop downs", function () {
