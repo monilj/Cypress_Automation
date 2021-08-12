c2c0354d152b9d066ed0cd80549288df8e6c0478
moniljoshi <monil.joshi@qualitia.com>
2021-08-12 07:09:46 +0530

    Added code to handle Alerts and confirmation boxes

---
+++ cypress/integration/2-advanced-examples/Web_UI_Controls_Hadnling_Alerts.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+describe("Execution on Web Control", function () {
+  it("Web Control Checkbox", function () {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("#alertbtn").click();
+    cy.get("#confirmbtn").click();
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal(
+        "Hello , share this practice page and share your knowledge"
+      );
+    });
+    cy.on("window:confirm", (str) => {
+      expect(str).to.equal("Hello , Are you sure you want to confirm?");
+    });
+  });
+});
