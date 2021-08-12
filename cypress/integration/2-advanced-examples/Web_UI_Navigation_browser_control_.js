97e3705f198c25e962611679d2c46eecd0a48e67
moniljoshi <monil.joshi@qualitia.com>
2021-08-12 15:26:28 +0530

    handling web tables using Cypress

--- cypress/integration/2-advanced-examples/Web_UI_Navigation_browser_control_.js
+++ cypress/integration/2-advanced-examples/Web_UI_Navigation_browser_control_.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe("Execution on Web Control", function () {
   it("Web Control for Handling Child tab with combination of Cypress and JQuery ", function () {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("#opentab").invoke("removeAttr", "target").click();
     cy.url().should("include", "qaclickacademy");
     cy.go("back");
