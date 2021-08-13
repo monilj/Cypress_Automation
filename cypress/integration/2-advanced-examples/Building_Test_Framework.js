b4c95e0ad33b5ab2a63b2efb5176b178e97b93de
moniljoshi <monil.joshi@qualitia.com>
2021-08-14 00:04:13 +0530

    Added PageObjects folder with HomePage objects and import it in the test

--- cypress/integration/2-advanced-examples/Building_Test_Framework.js
+++ cypress/integration/2-advanced-examples/Building_Test_Framework.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 import "cypress-iframe";
+
+import HomePage from "../pageObject/HomePage";
 describe("developing Test framework", function () {
   before(function () {
     cy.fixture("example").then(function (data) {
