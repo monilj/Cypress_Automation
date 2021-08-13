4758d21887b166e4e1e8400ea07aa32e262256c0
moniljoshi <monil.joshi@qualitia.com>
2021-08-13 07:26:33 +0530

    Validating attribute properties and thier behaviour

--- cypress/integration/2-advanced-examples/Building_Test_Framework.js
+++ cypress/integration/2-advanced-examples/Building_Test_Framework.js
@@ -8,7 +8,17 @@ describe("developing Test framework", function () {
   });
   it("Building Framework", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    cy.get("input[name='name']:nth-child(2)").type("this.data.name");
-    cy.get("select").select("this.data.gender");
+    cy.get("input[name='name']:nth-child(2)").type(this.data.name);
+    cy.get("select").select(this.data.gender);
+    cy.get(":nth-child(4) > .ng-untouched").should(
+      "have.value",
+      this.data.name
+    );
+    cy.get("input[name='name']:nth-child(2)").should(
+      "have.attr",
+      "minlength",
+      2
+    );
+    cy.get("#inlineRadio3").should("be.disabled");
   });
 });
