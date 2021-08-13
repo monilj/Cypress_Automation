137c89cd251cdd211de4387b5a6e5098c77e4d97
moniljoshi <monil.joshi@qualitia.com>
2021-08-13 07:07:29 +0530

    Data driver testing using Fixtures

--- cypress/integration/2-advanced-examples/Building_Test_Framework.js
+++ cypress/integration/2-advanced-examples/Building_Test_Framework.js
@@ -1,10 +1,14 @@
 /// <reference types="cypress" />
 import "cypress-iframe";
 describe("developing Test framework", function () {
-  before(function () {});
-  it("", function () {
+  before(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+  it("Building Framework", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    cy.get("input[name='name']:nth-child(2)").type("Bob");
-    cy.get("select").select("Female");
+    cy.get("input[name='name']:nth-child(2)").type("this.data.name");
+    cy.get("select").select("this.data.gender");
   });
 });
