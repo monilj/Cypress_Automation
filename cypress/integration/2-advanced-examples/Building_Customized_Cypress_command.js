652f9aff6ca0d2a5f638265140ef44f9fcd2349b
moniljoshi <monil.joshi@qualitia.com>
2021-08-13 22:42:29 +0530

    Building customized cypress commands for reuse

---
+++ cypress/integration/2-advanced-examples/Building_Customized_Cypress_command.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+import "cypress-iframe";
+describe("developing Test framework", function () {
+  before(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+  it("Building Framework", function () {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.selectProduct("Blackberry");
+    cy.selectProduct("Nokia Edge");
+  });
+});
